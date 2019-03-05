f7a67ec6e40f56e996c60b1dec4dfe3e4ba00a6d
FarrandTom <33008723+FarrandTom@users.noreply.github.com>
2019-03-05 11:06:56 +0000

    Initial commit

---
+++ public/sketch.js
@@ -0,0 +1,110 @@
+let img;
+let poseNet;
+let poses = [];
+
+function setup() {
+    img = createImg('data/dustin_johnson_setup.png', imageReady);
+}
+
+// when the image is ready, then load up the image dimensions and poseNet
+function imageReady(){
+    // set some options
+    let options = {
+        imageScaleFactor: 0.9,
+        minConfidence: 0.1
+    }
+
+    // Getting the original dimensions of the image
+    og_width = img.width;
+    og_height = img.height;
+    // Calculating the scaled and resized height and width of the image.
+    // All images maintain a fixed width.
+    aspect_ratio = og_height / og_width;
+    
+    width = 360;
+    height = aspect_ratio * width;
+
+    // The width and heigh ratios are used to scale the drawing of the keypoints
+    // and skeletion. 
+    width_ratio = width / og_width;
+    height_ratio = height / og_height;
+
+    // Canvas to draw image, and keypoints + skeletion onto.
+    createCanvas(width, height);
+
+    img.hide(); // hide the image in the browser
+    frameRate(1); // set the frameRate to 1 since we don't need it to be running quickly in this case
+
+    // assign poseNet
+    poseNet = ml5.poseNet(modelReady, options);
+    // This sets up an event that listens to 'pose' events
+    poseNet.on('pose', function (results) {
+        poses = results;
+        $.ajax({
+            type: "post",
+            url: "/poses",
+            data: JSON.stringify(results),
+            success: console.log('200'),
+            dataType: 'JSON',
+            contentType: 'application/json'
+          });
+    });
+}
+
+// when poseNet is ready, do the detection
+function modelReady() {
+    select('#status').html('Model Loaded');
+     
+    // When the model is ready, run the singlePose() function...
+    // If/When a pose is detected, poseNet.on('pose', ...) will be listening for the detection results 
+    // in the draw() loop, if there are any poses, then carry out the draw commands
+    poseNet.singlePose(img)
+}
+
+// draw() will not show anything until poses are found
+function draw() {
+    if (poses.length > 0) {
+        image(img, 0, 0, width, height);
+        drawSkeleton(poses);
+        drawKeypoints(poses);
+        noLoop(); // stop looping when the poses are estimated
+    }
+
+}
+
+// A function to draw ellipses over the detected keypoints
+function drawKeypoints() {
+    // Loop through all the poses detected
+    for (let i = 0; i < poses.length; i++) {
+        // For each pose detected, loop through all the keypoints
+        let pose = poses[i].pose;
+        for (let j = 0; j < pose.keypoints.length; j++) {
+            // A keypoint is an object describing a body part (like rightArm or leftShoulder)
+            let keypoint = pose.keypoints[j];
+            // Only draw an ellipse is the pose probability is bigger than 0.2
+            if (keypoint.score > 0.2) {
+                fill(255);
+                stroke(20);
+                strokeWeight(4);
+                ellipse(round(keypoint.position.x * width_ratio), round(keypoint.position.y * height_ratio), 8, 8);
+            }
+        }
+    }
+}
+
+// A function to draw the skeletons
+function drawSkeleton() {
+    // Loop through all the skeletons detected
+    for (let i = 0; i < poses.length; i++) {
+        let skeleton = poses[i].skeleton;
+        // For every skeleton, loop through all body connections
+        for (let j = 0; j < skeleton.length; j++) {
+            let partA = skeleton[j][0];
+            let partB = skeleton[j][1];
+            stroke(255);
+            strokeWeight(1);
+            line(partA.position.x * width_ratio, partA.position.y * height_ratio, 
+                 partB.position.x * width_ratio, partB.position.y * height_ratio);
+        }
+    }
+}
---
+++ server.js
@@ -0,0 +1,128 @@
+var express = require('express');
+var l2norm = require( 'compute-l2norm' );
+var bodyParser = require("body-parser");
+
+var app = express();
+var server = app.listen(3000);
+
+// Here we are configuring express to use body-parser as middle-ware.
+// This is so we can handle POST requests. 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use(express.static('public'));
+
+justin_rose_setup = [ 0.17970555707549749,
+  0.09236892609229846,
+  0.19049598104784266,
+  0.0859152670886969,
+  0.18144193426798524,
+  0.08386886339411645,
+  0.16432168459447394,
+  0.06289070948871929,
+  0.16880480191818856,
+  0.06895996584044545,
+  0.14077852641182892,
+  0.09600220110292149,
+  0.128904290872096,
+  0.10342838392798122,
+  0.12204716634023208,
+  0.16777081340533315,
+  0.12000816959904793,
+  0.17064071417678334,
+  0.13255993648104336,
+  0.2228372227880349,
+  0.13289191600526182,
+  0.22383787261607413,
+  0.04420532616356833,
+  0.18518281835265474,
+  0.05947606779363721,
+  0.1879432879369498,
+  0.07949649755336884,
+  0.2820087428268034,
+  0.08685021588348672,
+  0.2791258718156044,
+  0.08097705830905186,
+  0.3652642949162878,
+  0.07365678990129503,
+  0.37633378116507293,
+  0.9520760774612427,
+  0.13378602266311646,
+  0.9283162355422974,
+  0.055756017565727234,
+  0.9794830083847046,
+  0.5682874917984009,
+  0.8706150650978088,
+  0.45390671491622925,
+  0.9562039375305176,
+  0.7583954930305481,
+  0.6249509453773499,
+  0.6855019927024841,
+  0.8791465163230896,
+  0.3744296133518219,
+  0.708235502243042,
+  0.5363864302635193,
+  0.7279551029205322,
+  11.193432167172432 ]
+
+// poseVector1 and poseVector2 are 52-float vectors composed of:
+// Values 0-33: are x,y coordinates for 17 body parts in alphabetical order
+// Values 34-51: are confidence values for each of the 17 body parts in alphabetical order
+// Value 51: A sum of all the confidence values
+// Again the lower the number, the closer the distance
+function weightedDistanceMatching(poseVector1, poseVector2) {
+  let vector1PoseXY = poseVector1.slice(0, 34);
+  let vector1Confidences = poseVector1.slice(34, 51);
+  let vector1ConfidenceSum = poseVector1.slice(51, 52);
+
+  let vector2PoseXY = poseVector2.slice(0, 34);
+
+  // First summation
+  let summation1 = 1 / vector1ConfidenceSum;
+
+  // Second summation
+  let summation2 = 0;
+  for (let i = 0; i < vector1PoseXY.length; i++) {
+    let tempConf = Math.floor(i / 2);
+    let tempSum = vector1Confidences[tempConf] * Math.abs(vector1PoseXY[i] - vector2PoseXY[i]);
+    summation2 = summation2 + tempSum;
+  }
+
+  return summation1 * summation2;
+}
+
+
+app.post('/poses', (req, res) => {
+  keypoints = req.body[0]['pose']['keypoints']
+
+  var point;
+  var xy_array = [];
+  var confidence_array = [];
+
+  for (point in keypoints) {
+    x_position = keypoints[point]['position']['x'];
+    y_position = keypoints[point]['position']['y'];
+
+    xy_array.push(x_position);
+    xy_array.push(y_position);
+
+    single_confidence = keypoints[point]['score'];
+    confidence_array.push(single_confidence);
+  };
+
+  // Normalising the xy keypoint array using the L2 (euclidean) norm
+  var norm = l2norm(xy_array);
+  var norm_xy_array = xy_array.map(function(element) {
+    return element/norm;
+  });
+
+  confidence_sum = confidence_array.reduce((a, b) => a + b, 0);
+  confidence_array.push(confidence_sum);
+
+  var final_array = norm_xy_array.concat(confidence_array);
+
+  var result = weightedDistanceMatching(final_array, justin_rose_setup);
+  console.log(result);
+
+  res.send('Got a POST request')
+})
